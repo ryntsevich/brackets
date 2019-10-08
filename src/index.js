7331a0cf13fab587c014969e6fc86a019b78fc29
Ryntsevich Oksana <ryntsevich.oksana@gmail.com>
2019-10-08 12:02:08 +0300

    feat: implement task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('');    
+  for(let i = 0; i < strArr.length; i++){
+      for(let j = 0; j < bracketsConfig.length; j++){             
+          if(strArr[i] == bracketsConfig[j][0] && strArr[i+1] == bracketsConfig[j][1]){
+            strArr.splice(i,2)
+              i = -1;  
+          }  else {                   
+              continue;
+          }               
+      } 
+  }  
+  return strArr.length == 0;
 }
